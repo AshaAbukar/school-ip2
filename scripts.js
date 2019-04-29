ed5965ead82ee2ecb562528365060efddd2d822f
AshaAbukar <aisha63abubakar@gmail.com>
2019-04-29 10:31:30 +0300

    Add conditions for not a triangle.

--- scripts.js
+++ scripts.js
@@ -14,3 +14,6 @@ if(side1 <= 0 || side2 <= 0 || side3 <= 0) {
      else if(side1!==side2 || side2!==side3 || side3!==side1){
          alert("SCALENE TRIANGLE");
        }
+       else if(side1 + side2 <=side3 || side2 + side3 <=side1 || side1 + side3 <=side2){
+              alert("NOT A TRIANGLE");
+            }
