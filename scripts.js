3ba22bde41c070c38fe5a331181ae751b9fd31fc
AshaAbukar <aisha63abubakar@gmail.com>
2019-04-29 10:28:54 +0300

    Add conditions for equilateral triangle.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@ function myFunction (){
 var side1 = parseInt(document.getElementById("side1").value);
 var side2 = parseInt(document.getElementById("side2").value);
 var side3 = parseInt(document.getElementById("side3").value);
+if(side1 <= 0 || side2 <= 0 || side3 <= 0) {
+  alert("INVALID INPUT");
+     }
+     else if(side1===side2 && side2===side3 && side3===side1){
+         alert("EQUILATERAL TRIANGLE");
+       }
